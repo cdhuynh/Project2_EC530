cbb4b5e4563e82356c5da2c980aca4857589db0b
cdhuynh <56008239+cdhuynh@users.noreply.github.com>
2022-03-15 09:35:06 -0400

    Add files via upload

--- project2_api.py
+++ project2_api.py
@@ -1,10 +1,23 @@
 import json
 from pymongo import MongoClient
 from flask import Flask
+from flask_restful import Api, Resource
 
+
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+class all_messages(Resource):
+    def get(self):
+        return {"data": "This is a test for messages"}
+
+api.add_resource(all_messages, "/messages")
 
 # based on GFG implementation of storing items into MongoDB. Will proceed to create REstful API successful implementation. 
-
+'''
 with open('chat_message') as message:
     chat_message = json.load(message)
 
@@ -17,4 +30,6 @@ chat_client = database["data"]
 if isinstance(chat_message, list):
     chat_client.insert_many(chat_message)
 else:
-    chat_client.insert_one(chat_message)
+    chat_client.insert_one(chat_message) 
+
+    '''
