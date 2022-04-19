13da73e444985bfc423dee7da76f260ef84a9cd5
cdhuynh <56008239+cdhuynh@users.noreply.github.com>
2022-04-19 10:07:58 -0400

    Modified with validate schema json

--- admin_module.py
+++ admin_module.py
@@ -1,4 +1,5 @@
 import json
+import jsonschema
 from pymongo import MongoClient
 from flask import Flask
 from flask_restful import Api, Resource
@@ -16,9 +17,19 @@ class user_profile(Resource):
 
 api.add_resource(user_profile, "/admin")
 
-# based on GFG implementation of storing items into MongoDB. Will proceed to create REstful API successful implementation. 
 
 #For the admin module, want to try and separate the data via name, dob, age, etc. Parse information and then make it available/search by specified fields. 
+
+# Standard format for the json file is provided for comparison of received data. 
+
+standard_schema_admin = json.load("schema")
+
+# function for validating the admin data format as specified by the schema.json file.
+
+def validate_admin_format():
+    admin_data = json.loads("admin_test")
+    jsonschema.validate(admin_data, standard_schema_admin)
+    
 
 with open('admin_test') as data:
     admin_data = json.load(data)
@@ -29,8 +40,8 @@ database = client["admin_data"]
 
 admin_client = database["data"]
 
-if isinstance(user_data, list):
-    admin_client.insert_many(user_data)
+if isinstance(admin_data, list):
+    admin_client.insert_many(admin_data)
 else:
-    admin_client.insert_one(user_data) 
+    admin_client.insert_one(admin_data) 
 
